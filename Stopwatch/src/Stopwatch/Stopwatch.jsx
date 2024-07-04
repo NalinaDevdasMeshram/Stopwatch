2ba8b667006a269ded0b6b7c95507ca78d56cd73
nalina <naleenameshram85@gmail.com>
2024-07-04 21:43:04 +0530

    stopwatch completed

--- Stopwatch/src/Stopwatch/Stopwatch.jsx
+++ Stopwatch/src/Stopwatch/Stopwatch.jsx
@@ -1,7 +1,44 @@
-
+import { useEffect, useState } from "react"
+ import styles from './Stopwatch.module.css'
 const Stopwatch =()=>{
-    return (
-        <div>hello world start working on it</div>
-    )
+    const [time, setTime] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
+    useEffect(()=>{
+        let intervalId;
+        if(isRunning){
+            // starting time 0 to 1000 every 1 sec
+            intervalId = setInterval(()=>{setTime(prevstate => prevstate+1);
+
+            },1000)
+        }
+        return () => clearInterval (intervalId)
+    },[isRunning])
+    // method to start timer 
+     const startTime=()=>{
+        setIsRunning((prevstate)=>!prevstate)
+     }
+    // method to reset  timer back to 0
+     const ressetTime=()=>{
+        setTime(0);
+        // setIsRunning(false)
+     }
+     // time calculation 
+     // minutes calculation 
+      const minute = Math.floor(time/60);
+      // second calculation 
+       const second = (time%60);
+ return(
+    <div className={styles.container}>
+       <center><h1>StopWatch</h1></center>
+       <p className={styles.Stopwatch}> Time: {minute.toString().padStart(1, '0')} : {second.toString().padStart(2, '0')}</p>
+        <div className={styles.btncontainer}>
+            <button  className={`${styles.btntext}  ${isRunning? styles.stopbtntext:''}`} onClick={startTime}> {isRunning? 'Stop': 'Start'}</button>
+            <button  className ={styles.btn} onClick={ressetTime}>Reset</button> 
+        </div>
+            
+        
+    </div>
+ )
+   
 }
 export default Stopwatch
