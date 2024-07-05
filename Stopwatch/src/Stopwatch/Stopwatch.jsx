ece32ea67ce8f578e5c68b837a8dbf5211cd17bd
nalina <naleenameshram85@gmail.com>
2024-07-05 13:13:28 +0530

    done

--- Stopwatch/src/Stopwatch/Stopwatch.jsx
+++ Stopwatch/src/Stopwatch/Stopwatch.jsx
@@ -1,41 +1,43 @@
-import {useState,useEffect,useRef } from "react"
+import {useState,useEffect} from "react"
 //  import styles from './Stopwatch.module.css'
 const Stopwatch =()=>{
     const [time, setTime] = useState(0);
     const [isRunning, setIsRunning] = useState(false);
-    const intervalRef = useRef(null);
+    
  useEffect(()=>{
+    let intervalId;
         if(isRunning){
-        
+           
             // starting time 0 to 1000 every 1 sec
-           intervalRef. current = setInterval(()=>{setTime(prevstate => prevstate+1);
+           intervalId = setInterval(()=>{setTime(prevstate => prevstate+1);
           },1000);
-        } else if(!isRunning && intervalRef.current){
-             clearInterval(intervalRef.current)
-             intervalRef.current = null;
+        } else{
+             clearInterval(intervalId)
+             
         }
-        return ()=>clearInterval(intervalRef.current)
+        return ()=>clearInterval(intervalId)
     },[isRunning])
     // method to start timer 
-     const handleStopwatch=()=>{
+     const startStop=()=>{
         setIsRunning((prevstate)=>!prevstate)
-     }
+     };
     // method to reset  timer back to 0
      const resetTime=()=>{
+       setIsRunning(false)
         setTime(0);
-        setIsRunning(false)
     }
-     // time calculation 
-     // minutes calculation 
-      const minute = Math.floor(time/60);
-      // second calculation 
-       const second = (time%60);
+     const formatTime = (seconds)=>{
+        const minutes = Math.floor(seconds/60)
+        const remainingSecond = seconds % 60;
+        return `${minutes}:${remainingSecond < 10 ? "0":""}${remainingSecond}`
+     }
+      
  return(
     <div>
        <h1>StopWatch</h1>
-       <p> Time: {minute.toString().padStart(1, '0')} : {second.toString().padStart(2, '0')}</p>
+       <p> Time: {formatTime(time)}</p>
         <div>
-            <button onClick={handleStopwatch}>{isRunning ? 'stop' : 'start'}</button>
+            <button onClick={startStop}>{isRunning ? 'stop' : 'start'}</button>
             <button onClick={resetTime}>Reset</button> 
         </div>
     </div>
