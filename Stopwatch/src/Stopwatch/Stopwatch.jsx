d9d6fe4a72ec2e1a895f04e6b01e82d88b1dcc14
nalina <naleenameshram85@gmail.com>
2024-07-05 18:16:58 +0530

    working on

--- Stopwatch/src/Stopwatch/Stopwatch.jsx
+++ Stopwatch/src/Stopwatch/Stopwatch.jsx
@@ -9,23 +9,25 @@ const Stopwatch =()=>{
         if (isRunning){
              intervalId = setInterval(()=>{setTimeIn(prevstate => prevstate + 1);
            }, 1000);
-         } 
+         } else if(!isRunning && timeIn !==0){
+            clearInterval(intervalId)
+         }
         return () => clearInterval(intervalId)
-    },[isRunning])
+    },[isRunning,timeIn])
     // method to start timer 
      const startStop=()=>{
-        setIsRunning((previsRunning)=> !previsRunning)
+        setIsRunning(!isRunning)
      };
     // method to reset  timer back to 0
      const ResetTime = () => {
         setTimeIn(0);
-        setIsRunning(false);
+         setIsRunning(false);
     }
 
      const formatTime = (Inseconds)=>{
         const minutes = Math.floor(Inseconds/60);
         const remainingSecond = Inseconds % 60;
-        return `${minutes} : ${remainingSecond < 10 ? "0":""}${remainingSecond}`;
+        return `${minutes} : ${remainingSecond < 10 ? `0${remainingSecond}`: remainingSecond}`;
      }
     
     
