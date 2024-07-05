2a4a387c26977d31af4d8c2211d136bd99675dea
nalina <naleenameshram85@gmail.com>
2024-07-05 12:01:44 +0530

    changes done

--- Stopwatch/src/Stopwatch/Stopwatch.jsx
+++ Stopwatch/src/Stopwatch/Stopwatch.jsx
@@ -1,26 +1,47 @@
-import { useEffect, useState } from "react"
+import {useRef, useState } from "react"
  import styles from './Stopwatch.module.css'
 const Stopwatch =()=>{
     const [time, setTime] = useState(0);
     const [isRunning, setIsRunning] = useState(false);
- useEffect(()=>{
-        let intervalId;
-     if(isRunning){
-            // starting time 0 to 1000 every 1 sec
-            intervalId = setInterval(()=>{setTime(prevstate => prevstate+1);
+    const [startTime, setStartTime] = useState('start')
+    const timeRef = useRef(null)
+    const handleStopwatch =()=>{
+    if(isRunning){
+        // console.log(timeRef.current)
+        clearInterval(timeRef.current)
+        setStartTime('start');
+    }else{
+        timeRef.current = setInterval(() =>
+        {
+            setTime((prevState) => prevState + 1);
+            
+        },1000)
+        setStartTime('stop');
+    }
+    setIsRunning(!isRunning)
+};
 
-            },1000)
-        }
-         return () => clearInterval (intervalId)
-    },[isRunning, time])
-    // method to start timer 
-     const startTime=()=>{
-        setIsRunning((prevstate)=>!prevstate)
-     }
+//  useEffect(()=>{
+//         let intervalId ;
+//      if(isRunning){
+        
+//             // starting time 0 to 1000 every 1 sec
+//             intervalId = setInterval(()=>{setTime(prevstate => prevstate+1);
+
+//             },1000)
+//         }
+//         return () => clearInterval (intervalId)
+//     },[isRunning])
+//     // method to start timer 
+//      const startTime=()=>{
+//         setIsRunning((prevstate)=>!prevstate)
+//      }
     // method to reset  timer back to 0
-     const ressetTime=()=>{
-        setTime();
-         setIsRunning(false)
+     const resetTime=()=>{
+        clearInterval(timeRef.current)
+        setTime(0);
+        setIsRunning(false)
+        setStartTime('start')
      }
      // time calculation 
      // minutes calculation 
@@ -32,13 +53,11 @@ const Stopwatch =()=>{
        <center><h1>StopWatch</h1></center>
        <p className={styles.Stopwatch}> Time: {minute.toString().padStart(1, '0')} : {second.toString().padStart(2, '0')}</p>
         <div className={styles.btncontainer}>
-            <button  className={`${styles.btntext}  ${isRunning? styles.stopbtntext:''}`} onClick={startTime}> {isRunning? 'Stop': 'Start'}</button>
-            <button  className ={styles.btn} onClick={ressetTime}>Reset</button> 
+            <button  className={`${styles.btntext}  ${isRunning? styles.stopbtntext:''}`} onClick={handleStopwatch}> {startTime}</button>
+            <button  className ={styles.btn} onClick={resetTime}>Reset</button> 
+            </div>
         </div>
-            
-        
-    </div>
- )
+   )
    
 }
 export default Stopwatch
