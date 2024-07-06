bb5f79c783c0c6518e3668537e487391053fd1e7
nalina <naleenameshram85@gmail.com>
2024-07-06 16:04:27 +0530

    chsnges working

--- Stopwatch/src/Stopwatch/Stopwatch.jsx
+++ Stopwatch/src/Stopwatch/Stopwatch.jsx
@@ -2,19 +2,30 @@ import {useState, useRef} from "react"
 //  import styles from './Stopwatch.module.css'
 const formatTime = (time)=>{
    let stringData = time.current.innerHTML;
-   if(!stringData || !stringData.includes(':')){
-    stringData= "0:00"
-   }
-   let [minutes, seconds] = stringData.split(':').map(Number); 
-     seconds += 1;
-     if(seconds === 60){
-       minutes+=1;
-       seconds = 0;
+   let arr = stringData.split(':');
+   let minutes = arr[0];
+   let seconds = arr[1];
+   let mins = Number(minutes);
+   let secs = Number(seconds);
+     secs += 1;
+     if(secs === 60){
+       mins += 1;
+       secs = 0;
      }
-     const min = minutes.toString();
-     const sec = seconds.toString().padStart(2, '0')
+     let min1 = "";
+     let sec1 = "";
+ if(mins.toString().length < 2){
+   min1 = mins;
+ }else{
+   min1 = mins;
+ }
+ if(secs.toString().length < 2){
+   sec1 = "0" + secs;
+   }else{
+   sec1 = secs;
+ }
  
-     time.current.innerHTML = `${min}:${sec}`;
+     time.current.innerHTML = `${min1}:${sec1}`;
 }
 const Stopwatch =()=>{
     const [timeIn, setTimeIn] = useState(0);
@@ -27,7 +38,7 @@ const Stopwatch =()=>{
          e.target.innerHTML = "Stop"
          let t = valref
         let timer = setInterval(formatTime, 1000, t)
-         console.log(timer)
+         // console.log(timer)
          setTimeIn(timer)
       }else{
          e.target.innerHTML = 'Start'
