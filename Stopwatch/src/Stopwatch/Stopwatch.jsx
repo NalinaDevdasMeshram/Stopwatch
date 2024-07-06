8a74d8215ceef51a9274a98444974be8c8ca9457
nalina <naleenameshram85@gmail.com>
2024-07-06 14:07:59 +0530

    changes

--- Stopwatch/src/Stopwatch/Stopwatch.jsx
+++ Stopwatch/src/Stopwatch/Stopwatch.jsx
@@ -1,10 +1,17 @@
 import {useState, useEffect,useRef} from "react"
 //  import styles from './Stopwatch.module.css'
+const formatTime = (timeInSeconds)=>{
+   const minutes = Math.floor(timeInSeconds/60);
+   const remainingSecond = timeInSeconds % 60;
+ //   console.log(remainingSecond)
+ //   console.log('formatTime', `${minutes} : ${remainingSecond < 10 ? `0${remainingSecond}`: remainingSecond}`)
+   return `${minutes} : ${remainingSecond < 10 ? `0${remainingSecond}`: remainingSecond}`;
+}
 const Stopwatch =()=>{
     const [timeIn, setTimeIn] = useState(0);
     const [isRunning, setIsRunning] = useState(false);
     const intervalRef = useRef(null);
-    const valRef = useRef(null);
+    
     
  useEffect(() => {
        
@@ -12,43 +19,40 @@ const Stopwatch =()=>{
              intervalRef.current = setInterval(()=>{setTimeIn(prevstate => prevstate + 1);
            }, 1000);
          } 
+          else if(intervalRef.current){
+            clearInterval(intervalRef.current);
+          }
          //  console.log('isRunning, timeIn',  clearInterval(intervalId))
         return () => clearInterval(intervalRef.current) // clear in unmount 
        
     },[isRunning]);
-    useEffect(() => {
-      if (valRef.current) {
-         valRef.current.innerHTML = formatTime(timeIn);
-      }
-  }, [timeIn]);
-    // method to start timer 
-     const startStop=()=>{
-        setIsRunning(!isRunning)
-     };
+
+    const startStop =() =>{
+      setIsRunning(prevstate => !prevstate)
+    }
+    
     // method to reset  timer back to 0
    
      const ResetTime = () => {
          setTimeIn(0);
          setIsRunning(false);
-         // if(valRef.current){
-         //    valRef.current.innerHTML ="0:00";
-         // }
+         
     }
 
-     const formatTime = (timeIn)=>{
-        const minutes = Math.floor(timeIn/60);
-        const remainingSecond = timeIn % 60;
-      //   console.log(remainingSecond)
-      //   console.log('formatTime', `${minutes} : ${remainingSecond < 10 ? `0${remainingSecond}`: remainingSecond}`)
-        return `${minutes} : ${remainingSecond < 10 ? `0${remainingSecond}`: remainingSecond}`;
-     }
+   //   const formatTime = (timeIn)=>{
+   //      const minutes = Math.floor(timeIn/60);
+   //      const remainingSecond = timeIn % 60;
+   //    //   console.log(remainingSecond)
+   //    //   console.log('formatTime', `${minutes} : ${remainingSecond < 10 ? `0${remainingSecond}`: remainingSecond}`)
+   //      return `${minutes} : ${remainingSecond < 10 ? `0${remainingSecond}`: remainingSecond}`;
+   //   }
     
     
  return(
     <div>
        <h1>Stopwatch</h1>
-       <span>Time</span>
-       <span ref={valRef}>:{formatTime(timeIn)}</span>
+       <span>Time:</span>
+       <span>{formatTime(timeIn)}</span>
         <div>
             <button onClick={startStop}>
                 {isRunning ? 'Stop' : 'Start'}
