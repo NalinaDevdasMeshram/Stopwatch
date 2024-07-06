1911908f9f596d71d435479d58b7476925c47a3e
nalina <naleenameshram85@gmail.com>
2024-07-06 16:32:12 +0530

    chsnges

--- Stopwatch/src/Stopwatch/Stopwatch.jsx
+++ Stopwatch/src/Stopwatch/Stopwatch.jsx
@@ -1,68 +1,55 @@
-import {useState,useEffect, useRef} from "react"
+import {useState, useEffect,} from "react"
 //  import styles from './Stopwatch.module.css'
-const formatTime = (time)=>{
-   let stringData = time.current.innerHTML;
-   let [minutes, seconds] = stringData.split(':').map(Number);
-
-   seconds += 1;
-   if (seconds === 60) {
-       minutes += 1;
-       seconds = 0;
-   }
-
-   const min1 = minutes.toString();
-   const sec1 = seconds.toString().padStart(2, "0");
-
-   time.current.innerHTML = `${min1}:${sec1}`;
- 
-    
-}
 const Stopwatch =()=>{
-    const [timer, setTimer] = useState(null);
-    const timeref = useRef(null);
-
-    const handleStartStop = (e) =>{
-      const  val = e.target.innerHTML;
-      
-      if(val === 'Start'){
-         e.target.innerHTML = "Stop"
-         const t = timeref
-        const newTimer = setInterval( ()=>formatTime(t), 1000)
-         // console.log(timer)
-         setTimer(newTimer)
-      }else{
-         e.target.innerHTML = 'Start'
-         clearInterval(timer);
-         setTimer(null);
-      }
-    }
+    const [timeIn, setTimeIn] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
+   
     
+ useEffect(() => {
+        let intervalId;
+        
+        if (isRunning){
+             intervalId = setInterval(()=>{setTimeIn(prevstate => prevstate + 1);
+           }, 1000);
+         } 
+         // else if(!isRunning && timeIn !==0){
+         //    clearInterval(intervalId)
+         // }
+         //  console.log('isRunning, timeIn',  clearInterval(intervalId))
+        return () => clearInterval(intervalId) // clear in unmount 
+       
+    },[isRunning,timeIn])
+   
+    // method to start timer 
+     const startStop=()=>{
+        setIsRunning(!isRunning)
+     };
+    // method to reset  timer back to 0
+   
+     const ResetTime = () => {
+         setTimeIn(0);
+         setIsRunning(false);
+    }
 
-     const handleResetTime = ()=>{
-      timeref.current.innerHTML = "0:00";
-         if(timer){
-            clearInterval(timer);
-            setTimer(null);
-         }
+     const formatTime = (timeIn)=>{
+        const minutes = Math.floor(timeIn/60);
+        const remainingSecond = timeIn % 60;
+      //   console.log(remainingSecond)
+      //   console.log('formatTime', `${minutes} : ${remainingSecond < 10 ? `0${remainingSecond}`: remainingSecond}`)
+        return `${minutes} : ${remainingSecond < 10 ? `0${remainingSecond}`: remainingSecond}`;
      }
-     useEffect(() => {
-      return () => {
-          if (timer) {
-              clearInterval(timer);
-          }
-      };
-  }, [timer]);
+    
     
  return(
     <div>
        <h1>Stopwatch</h1>
-       <span>Time :</span>
-       <span ref={timeref}>0:00</span>
+       <span>Time</span>
+       <span>:{formatTime(timeIn)}</span>
         <div>
-            <button onClick={handleStartStop}>
-                Start
+            <button onClick={startStop}>
+                {isRunning ? 'Stop' : 'Start'}
             </button>
-            <button onClick={handleResetTime}>Reset</button> 
+            <button onClick={ResetTime} style={{margin:'10px'}}>Reset</button> 
         </div>
     </div>
    )
